36a1157760264fa600455e738fac13fed1c43c87
rayyy100 <91695098+rayyy100@users.noreply.github.com>
2021-09-30 13:22:10 -0600

    Create jokee.cpp

---
+++ jokee.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {	
+	
+	cout << "What do you call a fake noodle?" << endl << endl;
+
+	cout << "An im-pasta." << endl << endl;
+
+
+
+
+
+
+
+
+}
